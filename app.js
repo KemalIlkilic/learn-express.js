5de26db0f2aea41fa1494e750c4cf50e6fa9388d
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-07-17 23:04:15 +0300

    add public assets

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Express Tutorial')
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// setup static and middleware
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
+app.listen(5001, () => {
+  console.log("Server is listening on port 5001");
+});
